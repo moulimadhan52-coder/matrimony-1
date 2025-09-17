4226f5877407920d03c375b78cd9616897ebd67b
mouli madhan <moulimadhan2022its@jkkn.ac.in>
2025-09-17 14:32:46 +0530

    head

--- Home.js
+++ Home.js
@@ -1,8 +1,22 @@
 import "./Home.css"
 import logo from "./logo.png"
+import banner4 from "./banner4.jpg"
+const images = [
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_08_54_05Screenshot_20250812_142116_Gallery.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_07_28_10_58_00IMG-20250216-WA0073.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_01_57_55Kishore1.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_04_03_49_56b3a3de26-2271-4729-980b-8351810b9d43.jpeg&square=150",
+];
+const photos = [
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_08_54_05Screenshot_20250812_142116_Gallery.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_07_28_10_58_00IMG-20250216-WA0073.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_01_57_55Kishore1.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_04_03_49_56b3a3de26-2271-4729-980b-8351810b9d43.jpeg&square=150",
+];
+
 function Home(){
     return(
-        <div className="top">
+       <div className="body"> <div className="top">
         <div className="head">
             <div className="logo"><img src={logo}/></div>
             <div className="ctinfo"><h5 className="ph">+919443946541
@@ -87,9 +101,36 @@ function Home(){
                     <option>Mudhaliyar</option>
                 </select></div>
                 <div className="sb"><button className="searchpt">Search Partner</button></div>
-            </div></center>
+
             </div>
             
+            </center>
+            
+        </div>
+         <footer className="footer">
+        <div className="wp"><h1 className="welcome">WELCOME TO ANGALAMMAN MATRIMONT</h1>
+        <p>Sriangalamman Matrimony, is one of India’s leading online premium matrimonial websites which will help thousands of all Community members find their perfect life partner.
+
+Sriangalamman Matrimony  is the most trusted matrimony service for thousands who are in search of life partners in India & Worldwide.</p><a href="aj,zhxcas">...read more</a></div>
+<div className="wi"><img src={banner4}/></div>
+
+      </footer>
+      <footer className="brg"><div className="b&g">
+        <div><h1 className="prof">Brides & Grooms Profiles</h1><h1 className="u1">__________</h1><h1 className="u2">__________</h1></div> <div className="scroll-container">
+      <div className="scroll-track">
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`Profile ${index}`} className="scroll-image" />
+        ))}
+        
+        {images.map((src, index) => (
+          <img key={index + images.length} src={src} alt={`Profile duplicate ${index}`} className="scroll-image" />
+        ))}
+      </div>
+    </div></div>
+    
+      </footer>
+    
+            </div>
             
  )
 }
