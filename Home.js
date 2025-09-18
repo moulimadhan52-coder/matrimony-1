a20189c926f1f7e580ff402446730f52ddd1af44
mouli madhan <moulimadhan2022its@jkkn.ac.in>
2025-09-18 10:46:35 +0530

    head

--- Home.js
+++ Home.js
@@ -7,11 +7,11 @@ const images = [
   "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_01_57_55Kishore1.jpg&square=150",
   "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_04_03_49_56b3a3de26-2271-4729-980b-8351810b9d43.jpeg&square=150",
 ];
-const photos = [
-  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_08_54_05Screenshot_20250812_142116_Gallery.jpg&square=150",
-  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_07_28_10_58_00IMG-20250216-WA0073.jpg&square=150",
-  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_12_01_57_55Kishore1.jpg&square=150",
-  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_04_03_49_56b3a3de26-2271-4729-980b-8351810b9d43.jpeg&square=150",
+const images2 = [
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_30_10_16_16PHOTO-2025-08-30-15-44-56.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_09_04_01_07_52SHIVANI2.jpg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_08_30_06_56_35WhatsAppImage2025-05-24at7.05.41PM.jpeg&square=150",
+  "https://sriangalammanmatrimony.com/photoprocess.php?image=gallary/2025_09_15_08_31_57WhatsAppImage2025-09-15at1.55.58PM.jpeg&square=150",
 ];
 
 function Home(){
@@ -29,12 +29,19 @@ function Home(){
             </div>
             <div className="navbar">
                 <button className="navbt"><i class="fa fa-home" aria-hidden="true"></i>Home  |</button>
-                <button className="navbt"><i class="fa fa-user" aria-hidden="true"></i>About Us  |</button>
-                <button className="navbt"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign Up  |</button>
+                <select className="navbt"><option>About Us</option>
+                <option>FAQs</option>
+                <option>Terms & Conditions</option>
+                <option>Privacy Policy</option>
+                <option>Refund Policy</option>
+                <option>Disclaimer</option>
+                <option>Report Misissue</option></select>
+                <button className="navbt">|<i class="fa fa-sign-in" aria-hidden="true"></i>Sign Up  |</button>
                 <button className="navbt"><i class="fa fa-search" aria-hidden="true"></i>Search  |</button>
                 <button className="navbt"><i class="fa fa-tags" aria-hidden="true"></i>Membership  |</button>
                 <button className="navbt"><i class="fa fa-heart" aria-hidden="true"></i>Success story  |</button>
                 <button className="navbt"><i class="fa fa-map-marker" aria-hidden="true"></i>Contact Us</button>
+                
 
 
             </div>
@@ -61,7 +68,7 @@ function Home(){
                     <option>29</option>
                 </select></div>
                 <div className="lfr">
-                    <p>From Age</p>
+                    <p>To Age</p>
                 <select className="looking">
                     <option>28</option>
                     <option>29</option>
@@ -87,7 +94,7 @@ function Home(){
                     <option>Brahmin</option>
                 </select></div>
                 <div className="lfr">
-                    <p>Religion</p>
+                    <p>Caste</p>
                 <select className="looking">
                     <option>Any</option>
                     <option>Bramin</option>
@@ -126,11 +133,34 @@ Sriangalamman Matrimony  is the most trusted matrimony service for thousands who
           <img key={index + images.length} src={src} alt={`Profile duplicate ${index}`} className="scroll-image" />
         ))}
       </div>
-    </div></div>
+    </div><div className="scroll-container">
+        <div className="scroll-track">
+          {images2.map((src, index) => (
+            <img key={index} src={src} alt={`Profile ${index}`} className="scroll-image" />
+          ))}
+          {images2.map((src, index) => (
+            <img key={index + images2.length} src={src} alt={`Profile duplicate ${index}`} className="scroll-image" />
+          ))}
+        </div>
+      </div>
+      <div className="FAP"><h1 className="fap">Features & Policies</h1><h1 className="u1">__________</h1><h1 className="u2">__________</h1>
+      <div className="table">
+        <div className="ss"><div><button className="sb"><i class="fa fa-comments" aria-hidden="true"></i></button></div><div><h2>Free SMS & Chat</h2><p>An important facility such as chat is available in our site, so that people can interact with each other.</p></div></div>
+        <div className="ss"><div><button className="sb"><i class="fa fa-book" aria-hidden="true"></i></button></div><div><h2>Auto Match Maker</h2><p>Some new members get registered daily. This matrimony system updates and shows you matched profile.</p></div></div>
+        <div className="ss"><div><button className="sb"><i class="fa fa-users" aria-hidden="true"></i></button></div><div><h2>Recommend Profile</h2><p>when you follow some-one, you can see the updates of people whom you follow.</p></div></div>
+        <div className="ss"><div><button className="sb"><i class="fa fa-calendar" aria-hidden="true"></i></button></div><div><h2>Notification Alerts</h2><p>The activities such as changing the display picture, birthday notification, photo requests.</p></div></div>
+        <div className="ss"><div><button className="sb"><i class="fa fa-cog" aria-hidden="true"></i></button></div><div><h2>Restrictions Setting</h2><p>Restrictions Enable Copying Photos & Video Disable Anti Spam System Provided.</p></div></div>
+        <div className="ss"><div><button className="sb"><i class="fa fa-sticky-note-o" aria-hidden="true"></i></button></div><div><h2>Privacy Policy</h2><p>This electronic website is being operated and owned by sriangalammanmatrimony.com</p></div></div>
+        
+        </div></div>
+        <div className="sus"><div><h1 className="susp">Sucess Stories</h1><h1 className="u1">__________</h1><h1 className="u2">__________</h1></div></div>
+      
+    </div>
     
       </footer>
     
             </div>
+            
             
  )
 }
---
+++ dropdown.js
@@ -0,0 +1,15 @@
+import React from "react";
+const dropdown = () => {
+    return(
+        <div className="dropdown-container">
+            <div className="dropdown">
+                <ul className="droplist">
+                    <li className="items">profile</li>
+                    <li className="items">profile</li>
+                    <li className="items">profile</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default dropdown
